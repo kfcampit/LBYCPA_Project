b3a0dc416c1e8f704c6be8cc02df68b6a2a76851
kfcampit <75428470+kfcampit@users.noreply.github.com>
2021-05-25 03:01:49 +0800

    notebook v1

--- notebook.py
+++ notebook.py
@@ -6,12 +6,16 @@ class notebookMain:
     def __init__(self, root):
         self.root = root
         root.title("LBYCPA1 Project")
-        root.protocol("WM_DELETE_WINDOW", lambda : exit())
+        root.protocol("WM_DELETE_WINDOW", lambda: exit())
 
         mainframe = Frame(root)
-        mainframe.grid(column = 0, row = 0)
+        mainframe.grid(column = 0, row = 0, padx = 4, pady = 8)
 
-        Button(mainframe, text = "Pop - up", command = self.notebookPopUp).grid(column = 1, row = 1)
+        Label(mainframe, text = "Notes", font = Font(family = "Helvetica", size = 14, weight = "bold")).grid(columnspan = 3, column = 1, row = 1, pady = 4)
+
+        Button(mainframe, text = "View", command = self.notebookPopUp, width = 16).grid(column = 1, row = 2, pady = 4, padx = 8)
+        Button(mainframe, text = "Create", command = self.gotoNotebookCreate, width = 16).grid(column = 2, row = 2, pady = 4, padx = 8)
+        Button(mainframe, text = "Back", width = 16, command = self.back).grid(column = 3, row = 2, pady = 4, padx = 8)
 
         root.mainloop()
 
@@ -26,14 +30,20 @@ class notebookMain:
         Label(frameOne, text="hello")
 
         canvas = Canvas(root)
-        scroll_y = Scrollbar(root, orient="vertical", command=canvas.yview)
+        scroll_y = Scrollbar(root, orient = "vertical", command=canvas.yview)
 
         frame = Frame(canvas)
         
-        for i in range(1, 100):
-            Label(frame, text='test %i' % i, width = 48, justify = "left").grid(column = 0, row = i, padx = 8, pady = 8)
-            Button(frame, text = "test", width = 12).grid(column = 4, row = i, padx = 4)
-            Button(frame, text = "test", width = 12).grid(column = 5, row = i, padx = 4)
+        with open("data\\notes\\_master.txt", mode = "r", encoding = "utf8") as text:
+            textNames = text.readlines()
+            formatNames = [i[:-1] for i in textNames[:-1]]
+            formatNames.append(textNames[-1])
+
+        for i, fileName in enumerate(formatNames):
+            Label(frame, text = fileName.split(";")[1], width = 48, justify = "left").grid(column = 0, row = i, padx = 8, pady = 8)
+            Button(frame, text = "Edit", width = 12).grid(column = 4, row = i, padx = 4)
+            Button(frame, text = "Delete", width = 12).grid(column = 5, row = i, padx = 4)
+
         canvas.create_window(0, 0, anchor='nw', window=frame)
 
         canvas.update_idletasks()
@@ -43,10 +53,54 @@ class notebookMain:
         canvas.pack(fill='both', expand=True, side='left')
         scroll_y.pack(fill='y', side='right')
 
+
+    def gotoNotebookCreate(self, *args):
+        self.root.destroy()
+        self.new_window = Tk()
+        notebookCreate(self.new_window)
+
+    def back(self, *args):
+        self.root.destroy()
+        pass
+
 class notebookCreate:
     def __init__(self, root):
         self.root = root
         root.title("LBYCPA1 Project")
+        root.geometry("800x600+64+64")
+
+        root.protocol("WM_DELETE_WINDOW", lambda: exit())
+        mainframe = Frame(root)
+        mainframe.grid(column = 0, row = 0, padx = 8, pady = 8)
+
+        Label(mainframe, text = "Create Note", font = Font(family = "Helvetica", size = 16, weight = "bold")).grid(columnspan = 2, column = 1, row = 1, pady = 4, sticky = (W, E))
+
+        Label(mainframe, text = "Title:", font = Font(family = "Helvetica", size = 12)).grid(column = 1, row = 2, sticky = W)
+
+        self.titleText = StringVar()
+        Entry(mainframe, textvariable = self.titleText, width = 100).grid(column = 2, row = 2, padx = 4, sticky = W)
+
+        Label(mainframe, text = "Content:", font = Font(family = "Helvetica", size = 12)).grid(column = 1, row = 3, sticky = W)
+        
+        self.contentText = Text(mainframe, width = 96, height = 26)
+        self.contentText.grid(columnspan = 2, column = 1, row = 4, padx = 4)
+
+        Button(mainframe, text = "Save", width = 16, command = self.save).grid(column = 2, row = 5, padx = 4, sticky = E, pady = 8)
+        Button(mainframe, text = "Back", width = 16, command = self.back).grid(column = 1, row = 5, padx = 4, sticky = W, pady = 8)
+
+    def save(self, *args):
+        content = self.contentText.get("1.0",'end-1c')
+        fileName = self.titleText.get().lower().replace(" ", "_") + ".txt"
+        with open("data\\notes\\" + fileName, mode = "x", encoding = "utf8") as text:
+            text.writelines(self.titleText.get() + "\n" + content)
+        with open("data\\notes\\_master.txt", mode = "a", encoding = "utf8") as master:
+            master.write("\n" + fileName + ";" + self.titleText.get())
+        self.back
+
+    def back(self, *args):
+        self.root.destroy()
+        self.new_window = Tk()
+        notebookMain(self.new_window)
 
 class notebookEdit:
     def __init__(self, root):
