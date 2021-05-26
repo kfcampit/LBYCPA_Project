1ad8a3d1e55e9d63a7dadb3a6dcfa94ae7129c80
kfcampit <75428470+kfcampit@users.noreply.github.com>
2021-05-26 20:12:44 +0800

    Update enlist.py

--- enlist.py
+++ enlist.py
@@ -7,24 +7,57 @@ class enlistMain:
     def __init__(self, root):
         self.root = root
         root.title("LBYCPA1 Project")
-        root.geometry("800x600+64+64")
+        root.geometry("320x480+64+64")
 
         root.protocol("WM_DELETE_WINDOW", lambda: exit())
         mainframe = Frame(root)
         mainframe.grid(column = 0, row = 0, padx = 8, pady = 8)
 
-        Label(mainframe, text = "Create Note", font = Font(family = "Helvetica", size = 16, weight = "bold")).grid(columnspan = 2, column = 1, row = 1, pady = 4, sticky = (W, E))
+        Label(mainframe, text = "Enlistment", font = Font(family = "Helvetica", size = 16, weight = "bold")).grid(columnspan = 2, column = 1, row = 1, pady = 4, sticky = (W, E))
+        ttk.Separator(mainframe, orient = HORIZONTAL).grid(columnspan = 2, column = 1, row = 2, ipadx = 140, pady = 16)
+
+        Label(mainframe, text = "Search Courses", font = Font(family = "Helvetica", size = 12, weight = "bold")).grid(columnspan = 2, column = 1, row = 3, pady = 4, sticky = (W, E))
+        Label(mainframe, text = "Enter course code", font = Font(family = "Helvetica", size = 8, slant = "italic")).grid(columnspan = 2, column = 1, row = 4, pady = 4, sticky = (W, E))
         
         self.courseCode = StringVar()
-        ttk.Entry(mainframe, textvariable = self.courseCode, width = 40).grid(column = 1, row = 2, padx = 8, sticky = W)
-        Button(mainframe, text = "Search", command = self.coursePopup).grid(column = 2, row = 2)
+        ttk.Entry(mainframe, textvariable = self.courseCode, width = 24).grid(column = 1, row = 5, padx = 2, sticky = W)
+        ttk.Button(mainframe, text = "Search", command = self.coursePopup, width = 16).grid(column = 2, row = 5, sticky = E)
+
+        ttk.Separator(mainframe, orient = HORIZONTAL).grid(columnspan = 2, column = 1, row = 6, ipadx = 140, pady = 16)
+        Label(mainframe, text = "Add Courses", font = Font(family = "Helvetica", size = 12, weight = "bold")).grid(columnspan = 2, column = 1, row = 7, pady = 4, sticky = (W, E))
+        Label(mainframe, text = "Enter course number", font = Font(family = "Helvetica", size = 8, slant = "italic")).grid(columnspan = 2, column = 1, row = 8, pady = 4, sticky = (W, E))
+
+        self.courseNo = StringVar()
+        ttk.Entry(mainframe, textvariable = self.courseNo, width = 24).grid(column = 1, row = 9, padx = 2, sticky = W)
+        ttk.Button(mainframe, text = "Check", command = self.getData, width = 16).grid(column = 2, row = 9, pady = 8, sticky = E)
+
+        self.courseName = StringVar()
+        Label(mainframe, textvariable = self.courseName, width = 32, font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "groove").grid(columnspan = 2, column = 1, row = 10, sticky = (W, E))
         
+        self.facultyName = StringVar()
+        Label(mainframe, textvariable = self.facultyName, width = 32, font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "groove").grid(columnspan = 2, column = 1, row = 11, sticky = (W, E))
+
+        self.courseCde = StringVar()
+        Label(mainframe, textvariable = self.courseCde, width = 16, font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "groove").grid(column = 1, row = 12, sticky = (W, E))
+
+        self.section = StringVar()
+        Label(mainframe, textvariable = self.section, width = 16, font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "groove").grid(column = 2, row = 12, sticky = (W, E))
         
+        self.day = StringVar()
+        Label(mainframe, textvariable = self.day, width = 16, font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "groove").grid(column = 1, row = 13, sticky = (W, E))
+
+        self.time = StringVar()
+        Label(mainframe, textvariable = self.time, width = 16, font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "groove").grid(column = 2, row = 13, sticky = (W, E))
+
+        Label(mainframe).grid(column = 1, row = 14, sticky = W, pady = 8)
+        self.saveButton = ttk.Button(mainframe, text = "Save", width = 16, command = self.back)
+        ttk.Button(mainframe, text = "View Calendar", width = 20, command = self.back).grid(column = 2, row = 15, sticky = E, pady = 4)
+        ttk.Button(mainframe, text = "Back", width = 16, command = self.back).grid(column = 1, row = 15, padx = 4, sticky = W, pady = 4)
         
     def coursePopup(self):
         root = Toplevel()
         root.title("LBYCPA1 Project")
-        root.geometry("800x220")
+        root.geometry("1100x200")
 
         canvas = Canvas(root)
         scroll_y = ttk.Scrollbar(root, orient="vertical", command=canvas.yview)
@@ -37,14 +70,23 @@ class enlistMain:
             self.result.append([str(int(rows["CLASS NO"])), rows["COURSE CODE"], rows["COURSE TITLE"], rows["OFFERED to"], rows["SEC"], rows["FACULTY"], rows["DAY1"], str(int(rows["BEGIN1"])), str(int(rows["END1"]))])
 
         frame = Frame(canvas)
+
+        Label(frame, text = "NO.", width = 8, justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge", bg = "grey").grid(column = 0, row = 0, sticky = W)
+        Label(frame, text = "CODE", width = 12, justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge", bg = "grey").grid(column = 1, row = 0, sticky = W)
+        Label(frame, text = "COURSE TITLE", justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge", width = 32, bg = "grey").grid(column = 2, row = 0, sticky = W)
+        Label(frame, text = "SEC", justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge", width = 8, bg = "grey").grid(column = 3, row = 0, sticky = W)
+        Label(frame, text = "FACULTY", justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge", width = 32, bg = "grey").grid(column = 4, row = 0, sticky = W)
+        Label(frame, text = "DAY", justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge", width = 8, bg = "grey").grid(column = 5, row = 0, sticky = W)
+        Label(frame, text = "TIME", justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge", width = 16, bg = "grey").grid(column = 6, row = 0, sticky = W)
+
         for i in range(len(self.result)):
-            Label(frame, text = self.result[i][0], width = 8, justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge").grid(column = 0, row = i, sticky = W)
-            Label(frame, text = self.result[i][1], width = 12, justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge").grid(column = 1, row = i, sticky = W)
-            Label(frame, text = self.result[i][2], justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge", padx = 8).grid(column = 2, row = i, sticky = W)
-            Label(frame, text = (self.result[i][3] + self.result[i][4]), justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge", width = 8).grid(column = 3, row = i, sticky = W)
-            Label(frame, text = self.result[i][5], justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge", width = 32).grid(column = 4, row = i, sticky = W)
-            Label(frame, text = self.result[i][6], justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge", width = 8).grid(column = 5, row = i, sticky = W)
-            Label(frame, text = (self.result[i][7] + " - " + self.result[i][8]), justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge", width = 16).grid(column = 6, row = i, sticky = W)
+            Label(frame, text = self.result[i][0], width = 8, justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge").grid(column = 0, row = i + 1, sticky = W)
+            Label(frame, text = self.result[i][1], width = 12, justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge").grid(column = 1, row = i + 1, sticky = W)
+            Label(frame, text = self.result[i][2], justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge", width = 32).grid(column = 2, row = i + 1, sticky = W)
+            Label(frame, text = (self.result[i][3] + self.result[i][4]), justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge", width = 8).grid(column = 3, row = i + 1, sticky = W)
+            Label(frame, text = self.result[i][5], justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge", width = 32).grid(column = 4, row = i + 1, sticky = W)
+            Label(frame, text = self.result[i][6], justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge", width = 8).grid(column = 5, row = i + 1, sticky = W)
+            Label(frame, text = (self.result[i][7] + " - " + self.result[i][8]), justify = "left", font = Font(size = 12, family =  "Helvetica"), borderwidth = 1, relief = "ridge", width = 16).grid(column = 6, row = i + 1, sticky = W)
 
         canvas.create_window(0, 0, anchor='nw', window=frame)
 
@@ -55,7 +97,30 @@ class enlistMain:
         canvas.pack(fill='both', expand=True, side='left')
         scroll_y.pack(fill='y', side='right')
 
+    def getData(self):
+        self.read = pd.read_csv('data\\database\\gcoe_t2_2021_database.csv')
+        searched = self.read.loc[self.read["CLASS NO"] == int(self.courseNo.get()), ["COURSE CODE", "COURSE TITLE", "OFFERED to", "SEC", "FACULTY", "DAY1", "BEGIN1", "END1"]]
+        self.resultDetails = searched.values.tolist()[0]
+        
+        self.courseName.set(self.resultDetails[1])
+        self.facultyName.set(self.resultDetails[4])
+        self.courseCde.set(self.resultDetails[0])
+        self.section.set(self.resultDetails[2] + self.resultDetails[3])
+        self.day.set(self.resultDetails[5])
+        self.time.set(str(int(self.resultDetails[6])) + " - " + str(int(self.resultDetails[7])))
 
+        self.saveButton.grid(column = 2, row = 14, padx = 4, sticky = E, pady = 8)
+
+    def back(self, *args):
+        self.root.destroy()
+
+
+
+
+        
+
+
+        
 
 
 root = Tk()
